02bc119b5fd5f4b603b4d9db37eb0de02e8942df
Abhishek9938 <Abhishek.jupalli@Cubastion.com>
2025-10-15 13:34:16 +0530

    Update print.js
    update the print js url to uatjutecomm

--- frappe/printing/page/print/print.js
+++ frappe/printing/page/print/print.js
@@ -219,7 +219,7 @@ frappe.ui.form.PrintView = class {
 			let btn = this.page.add_button(
 				__("eSign"),
 				() => {
-					const url = "https://esigndashboard.cdac.in/asponboardingservice/login";
+					const url = "https://uatjutesmart.jutecomm.gov.in/SpringBootESign/";
 					let w = window.open(url, "_blank");
 					if (!w) {
 						frappe.msgprint(__("Please enable pop-ups"));
